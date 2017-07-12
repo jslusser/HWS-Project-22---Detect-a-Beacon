f5477f69b56b0db342a467223e07e7e589d1cf42
jslusser <jslusser@gmail.com>
2017-07-12 11:13:50 -0400

    Initial commit

---
+++ Project22/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by James Slusser on 7/12/17.
+//  Copyright © 2017 James Slusser. All rights reserved.
+//  https://www.hackingwithswift.com/read/22/overview
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+
+    @IBOutlet weak var distanceReading: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        
+        view.backgroundColor = UIColor.gray
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
